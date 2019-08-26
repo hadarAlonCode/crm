cb07de13a0a22c1bd76ab60b47e0654cf501538b
Hadar Alon <hadaralon3@gmail.com>
2019-08-26 12:49:09 +0300

    push to heroku

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-// app.use(express.static(path.join(__dirname, 'src'))) 
-// app.use(express.static(path.join(__dirname, 'node_modules')))
+app.use(express.static(path.join(__dirname, 'src'))) 
+app.use(express.static(path.join(__dirname, 'node_modules')))
 
 // app.use(function (req, res, next) {
 //     res.header('Access-Control-Allow-Origin', '*')
